7e975addfa7e4eaf48cefd42b0892a169fb75c3d
phieutran <phieu@flood.io>
2020-06-30 17:59:29 +0700

    Fixing cors

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,16 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(authRoute);
 app.use(itemRoute);
 app.use(meRoute);
